b6746f77eba305b3dccaebdf0964bce383c3fabf
tamiraminh <tamiramin.hayat@evermos.com>
2023-07-26 11:33:55 +0700

    add Read 1 more input integer x and print the appended string x times functionality

--- main.go
+++ main.go
@@ -10,6 +10,13 @@ func appendString(s1 string, s2 string) string  {
 	return s1 + s2
 }
 
+func printNTimes(s string, n int)  {
+	for i := 0 ; i < n ; i++ {
+		fmt.Println(s)
+	} 
+	
+}
+
 func main() {
 	var w1, w2 string
 	fmt.Println("Input word one: ")
@@ -29,6 +36,10 @@ func main() {
 	sentence := appendString(w1, w2)
 	printString(sentence)
 
-	
+	var n int 
+	fmt.Println()
+	fmt.Println("Input how many will print combined string: ")
+	fmt.Scanf("%d", &n)
+	printNTimes(sentence, n)
 
 }
