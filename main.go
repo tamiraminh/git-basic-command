e15ded027fc1492dcf44d1f8e8e3325cea6bebb4
tamiraminh <13519129@std.stei.itb.ac.id>
2023-07-26 11:23:58 +0700

    add Append the string and print the value functionality

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ func printString(s string) {
 	fmt.Println(s)
 }
 
+func appendString(s1 string, s2 string) string  {
+	return s1 + s2
+}
+
 func main() {
 	var w1, w2 string
 	fmt.Println("Input word one: ")
@@ -14,8 +18,17 @@ func main() {
 	fmt.Scanln(&w2)
 
 	fmt.Println()
-	
+
 	fmt.Println("inputed string")
 	printString(w1)
 	printString(w2)
+
+	fmt.Println()
+
+	fmt.Println("Gabungan kedua String: ")
+	sentence := appendString(w1, w2)
+	printString(sentence)
+
+	
+
 }
